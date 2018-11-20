1ea49da32c19c17e153c5ebaa918adc9c200f031
Bitz3d <raf.cieslinski@gmail.com>
2018-11-20 19:49:17 +0100

    LoginControlelr

--- src/main/java/pl/rafalab/rafalworld/Controller/LoginController.java
+++ src/main/java/pl/rafalab/rafalworld/Controller/LoginController.java
@@ -2,7 +2,18 @@ package pl.rafalab.rafalworld.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.ws.rs.GET;
 
 @Controller
 public class LoginController {
+
+    @GET
+    @RequestMapping(value = "/login")
+    public String showLoginPage(){
+
+        return "login";
+    }
+
 }
