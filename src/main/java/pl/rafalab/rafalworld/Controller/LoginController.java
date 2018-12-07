35ddb7d3d0ceb2e8722c1c2edffa999ece06d1b6
Bitz3d <raf.cieslinski@gmail.com>
2018-12-07 19:31:44 +0100

    Paginacja start

--- src/main/java/pl/rafalab/rafalworld/Controller/LoginController.java
+++ src/main/java/pl/rafalab/rafalworld/Controller/LoginController.java
@@ -1,19 +1,14 @@
 package pl.rafalab.rafalworld.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.ws.rs.GET;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
-    @GET
-    @RequestMapping(value = "/login")
+  
+    
+    @GetMapping(value = "/login")
     public String showLoginPage(){
-
         return "login";
     }
-
-
 }
