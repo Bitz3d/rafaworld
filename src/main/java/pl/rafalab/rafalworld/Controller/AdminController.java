76ea756675e147213e67a5bf57710220a3a81ca0
Bitz3d <raf.cieslinski@gmail.com>
2018-12-08 19:02:38 +0100

    Edit user page

--- src/main/java/pl/rafalab/rafalworld/Controller/AdminController.java
+++ src/main/java/pl/rafalab/rafalworld/Controller/AdminController.java
@@ -1,24 +1,44 @@
 package pl.rafalab.rafalworld.Controller;
 
+import java.lang.ProcessBuilder.Redirect;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.ws.rs.GET;
+
+import org.apache.logging.log4j.message.Message;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.rafalab.rafalworld.Model.User;
 import pl.rafalab.rafalworld.Services.AdminService;
+import pl.rafalab.rafalworld.Services.UserService;
 
 @Controller
 public class AdminController{
 
+	private int ELEMENTS =5;
+	
 	@Autowired
 	private AdminService adminService;
+	
+	@Autowired
+	private UserService userServices;
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GET
 	@RequestMapping(value="/admin")
@@ -37,22 +57,67 @@ public class AdminController{
  		model.addAttribute("userList",userList);
  		model.addAttribute("numberOfPages",numberOfPages);
  		model.addAttribute("currentPage",currentPage + 1);
+ 		model.addAttribute("recordStartCounter",currentPage * ELEMENTS);
+ 		
 		return "admin/users";
+	}
+	
+	@GetMapping(value="/admin/users/edit/{id}")
+	@Secured(value={"ROLE_ADMIN"})
+	public String edituser(@PathVariable("id") int id, Model model)
+	{		
+		User user = userServices.findUserById(id);
+		Map<Integer, String> roleMap = prepareRoleMap();
+		Map<Integer, String> actvieMap = prepareActiveMap();
+		int role = user.getRoles().iterator().next().getId();
+		user.setNrRoli(role);
+		model.addAttribute("roleMap", roleMap);
+		model.addAttribute("actvieMap", actvieMap);
+		model.addAttribute("user", user);
+		
+		return "admin/useredit"; 
+	}
+	
+	
+	@PostMapping(value="/admin/updateuser/{id}")
+	@Secured(value={"ROLE_ADMIN"})
+	public String updateUser(@PathVariable("id") long id, User user){
+		int roleNumber = user.getNrRoli();
+		int activity = user.getActive();
+		adminService.updateUser(id, roleNumber, activity);
+		
+		return "redirect:admin/user/1";
 	}
 	
 	
 	
 	//helper methods
 	private Page<User> getAllUsersPageable(int page) {
-		int element =5;
-		Page<User> pages = adminService.findAll(PageRequest.of(page, element)); 
+		Page<User> pages = adminService.findAll(PageRequest.of(page, ELEMENTS)); 
 		pages.forEach(u -> {
 			int roleNumber = u.getRoles().iterator().next().getId();
 			u.setNrRoli(roleNumber);
 		});
 		
-		return pages;
-		
+		return pages;		
 	}
+	
+	private Map<Integer, String> prepareRoleMap() {
+			Locale locale = Locale.getDefault();
+			Map<Integer, String> roleMap = new HashMap<Integer, String>();
+			roleMap.put(1, messageSource.getMessage("word.admin", null, locale));
+			roleMap.put(2, messageSource.getMessage("word.user", null, locale));
+			
+			return roleMap;
+	}
+	
+	private Map<Integer, String> prepareActiveMap() {
+		Locale locale = Locale.getDefault();
+		Map<Integer, String> activeMap = new HashMap<Integer, String>();
+		activeMap.put(0, messageSource.getMessage("word.nie", null, locale));
+		activeMap.put(1, messageSource.getMessage("word.tak", null, locale));
+		
+		return activeMap;
+}
 	
 }
--- src/main/java/pl/rafalab/rafalworld/Repositories/AdminRepository.java
+++ src/main/java/pl/rafalab/rafalworld/Repositories/AdminRepository.java
@@ -1,9 +1,21 @@
 package pl.rafalab.rafalworld.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import pl.rafalab.rafalworld.Model.User;
 
 public interface AdminRepository extends JpaRepository<User, Long>  {
 
 	
+	@Modifying
+	@Query("UPDATE User u SET u.active =:intAvtive where u.id = :id")
+	void updateUserAvtivity(@Param("intAvtive") int intAvtive, @Param("id") long id);
+	
+
+	@Modifying
+	@Query(value="UPDATE role r SET r.role.id=:roleID where r.user_id=:id", nativeQuery=true)
+	void updateUserRole(@Param("roleID") int intAvtive, @Param("id") long id);
 } 
