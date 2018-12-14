9367280da39539f2a7b51c3f8b3c45493305c26d
Bitz3d <raf.cieslinski@gmail.com>
2018-12-14 19:34:05 +0100

    Butch to database

--- src/main/java/pl/rafalab/rafalworld/Services/AdminService.java
+++ src/main/java/pl/rafalab/rafalworld/Services/AdminService.java
@@ -1,6 +1,8 @@
 package pl.rafalab.rafalworld.Services;
 
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -12,5 +14,6 @@ public interface AdminService {
 	Page<User> findAll(Pageable pageable); 
 	void updateUser(long id, int roleNumber, int activity);
 	Page<User> searchUsers(String param, Pageable pageable);
+	void insertBatch(List<User> userList);
 	
 }
--- src/main/java/pl/rafalab/rafalworld/configuration/WebMvcConfig.java
+++ src/main/java/pl/rafalab/rafalworld/configuration/WebMvcConfig.java
@@ -1,5 +1,8 @@
 package pl.rafalab.rafalworld.configuration;
 
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,14 +11,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
     @Bean
     public BCryptPasswordEncoder pwdEncrypt(){
-
         BCryptPasswordEncoder bcp = new BCryptPasswordEncoder();
-
         return  bcp;
-
     }
+// Obsluga plików do 5MB
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+    	MultipartConfigFactory configFactory = new MultipartConfigFactory();
+    	configFactory.setMaxFileSize("5MB");
+    	configFactory.setMaxRequestSize("5MB");
+    	return configFactory.createMultipartConfig();
+    }
+    
 
 }
